964738f868c3d17b1a0f32e4f26c751988eee42b
Павел Пищик <6317693753@my.lerna.by>
2026-04-04 12:26:33 +0300

    Delete prowerka.py

--- prowerka.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-
-driver = webdriver.Remote(
-    command_executor="http://127.0.0.1:4444",
-    options=webdriver.ChromeOptions()
-)
-driver.get("https://google.com")
-driver.quit()
